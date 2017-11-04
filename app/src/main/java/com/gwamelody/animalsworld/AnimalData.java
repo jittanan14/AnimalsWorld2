ff0396e92d2c59710712474e2ce7a10ab7afd68d
Jittanan Gwa <Jittanan56@gmail.com>
2017-11-04 20:55:57 +0700

    animal

---
+++ app/src/main/java/com/gwamelody/animalsworld/AnimalData.java
@@ -0,0 +1,24 @@
+package com.gwamelody.animalsworld;
+
+import com.gwamelody.animalsworld.model.Animal;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Promlert on 2017-10-29.
+ */
+
+public class AnimalData {
+
+    private static AnimalData sInstance;
+
+    public ArrayList<Animal> animalList;
+
+    public static AnimalData getInstance() {
+        if (sInstance == null) {
+            sInstance = new AnimalData();
+        }
+        return sInstance;
+    }
+
+}
